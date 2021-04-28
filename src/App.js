3b0fa34cd1cba109b6304f36ed7618a61f236ca5
muhnnadhabib1 <muhnnad002017@gmail.com>
2021-04-29 00:34:27 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@
 import './App.css';
 import Header from "./components/Header"
 import MemeGenerator from "./components/MemeGenerator"
-
+import MidiPlayer from 'midi-player-js';
+import  midiFile from "./test.mid"
+const Player = new MidiPlayer.Player();
 
 function App() {
 
+
+  Player.loadFile(midiFile);
+  Player.play();
 
   return (
     <div dir="rtl" className="App">
